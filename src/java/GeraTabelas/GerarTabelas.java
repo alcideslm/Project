a8f2773afc0445f6ca292114f2711e82be065887
mdogomes <matheusdeoliveiragomes@outlook.com>
2017-10-25 20:36:45 -0200

    Acrescentando models

--- src/java/GeraTabelas/GerarTabelas.java
+++ src/java/GeraTabelas/GerarTabelas.java
@@ -15,12 +15,16 @@ import javax.persistence.Persistence;
  */
 public class GerarTabelas {
     public static void main(String[] args) {
-        EntityManagerFactory em = Persistence.
-            createEntityManagerFactory("WebApplication-Spring-3anoPU");
+        try{
+            EntityManagerFactory em = Persistence.
+            createEntityManagerFactory("WebApplication1PU");
             
     System.out.println("Criando tabelas no banco...");
     
     em.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
     
 }
