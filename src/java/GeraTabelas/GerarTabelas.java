6982d444e17ec95281806aa21a028c50a557d0ad
Alcides Lemos <alcideslm1@gmail.com>
2017-11-01 20:31:26 -0200

    Arrumei essa baguaça

--- src/java/GeraTabelas/GerarTabelas.java
+++ src/java/GeraTabelas/GerarTabelas.java
@@ -16,13 +16,12 @@ public class GerarTabelas {
     public static void main(String[] args) {
         try{
             EntityManagerFactory em = Persistence.
-            createEntityManagerFactory("WebApplication1PU");
-            
-    System.out.println("Criando tabelas no banco...");
-    
-    em.close();
+            createEntityManagerFactory("tarefa"); 
+            System.out.println("Criando tabelas no banco...");
+            em.close();
         }catch (Exception e){
             System.out.println(e.getMessage());
+            System.out.println("teste");
         }
     }
     
